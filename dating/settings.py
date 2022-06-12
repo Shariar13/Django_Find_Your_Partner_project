de37013ead08d1d3ac58692adc1fd4abfffd458e
Shariar13 <88611911+Shariar13@users.noreply.github.com>
2022-06-13 00:25:01 +0600

    Update settings.py

--- dating/settings.py
+++ dating/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'                                     
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'createmysite.pw@gmail.com'
-EMAIL_HOST_PASSWORD = '01986292330@sK'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
